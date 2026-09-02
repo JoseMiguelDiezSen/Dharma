f97725a51bbd5afa90a5e0fa966280ff3155aaef
Jose Miguel Diez Sen <josemigueldiezsen@gmail.com>
2026-09-02 23:48:21 +0200

    Caambios

--- Dharma/WeatherForecast.cs
+++ Dharma/WeatherForecast.cs
@@ -6,6 +6,7 @@ namespace Dharma
 
         public int TemperatureC { get; set; }
 
+
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
