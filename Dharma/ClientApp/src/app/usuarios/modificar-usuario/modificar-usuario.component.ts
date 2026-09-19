6024f8947b4eb003d190617d5967ac378d7c722d
Jose Miguel Diez <73285524+JoseMiguelDiezSen@users.noreply.github.com>
2026-09-19 11:01:16 +0200

    Se corrige modificar

--- Dharma/ClientApp/src/app/usuarios/modificar-usuario/modificar-usuario.component.ts
+++ Dharma/ClientApp/src/app/usuarios/modificar-usuario/modificar-usuario.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnChanges } from '@angular/core';
+// Importamos Component, Input, Output, EventEmitter y OnChanges desde el núcleo de Angular
+import { Component, Input, Output, EventEmitter, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-modificar-usuario',
@@ -14,6 +15,18 @@ export class ModificarUsuarioComponent implements OnChanges {
 
   ngOnChanges(): void {
     this.modalModificarUsuario = this.abrir;
+  }
+
+  // @Output(): emisor de eventos para avisar al componente padre
+  @Output() cerrar = new EventEmitter<void>();
+
+  // Método que oculta el modal y emite el evento al padre
+  cerrarModal(): void {
+    // 1. Oculta el modal en este componente
+    this.modalModificarUsuario = false;
+
+    // 2. Notifica al componente padre para que ponga su variable a false
+    this.cerrar.emit();
   }
 
   editUser(): void {
